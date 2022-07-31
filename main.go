245d372249b44bcc020105f82575c5eda717c8fc
Tuan Pham Anh <baabeetaa@gmail.com>
2022-07-31 11:44:26 +0700

    ...

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	tmdb "github.com/tendermint/tm-db"
 	"os"
+	"path/filepath"
 	"runtime"
 )
 
@@ -14,12 +15,17 @@ func main() {
 
 	fmt.Printf("source=%s, target=%s\n", os.Args[1], os.Args[2])
 
-	dbLev, errLev := tmdb.NewGoLevelDB(os.Args[2], os.Args[1])
+	dbName := filepath.Base(os.Args[1])
+	dbName = dbName[:len(dbName)-len(filepath.Ext(dbName))] // get rid of .db
+	dbDirSource := filepath.Dir(os.Args[1])
+	dbDirTarget := os.Args[2]
+
+	dbLev, errLev := tmdb.NewGoLevelDB(dbName, dbDirSource)
 	if errLev != nil {
 		panic(errLev)
 	}
 
-	dbPeb, errPeb := tmdb.NewPebbleDB(os.Args[2], ".")
+	dbPeb, errPeb := tmdb.NewPebbleDB(dbName, dbDirTarget)
 
 	if errPeb != nil {
 		panic(errPeb)
