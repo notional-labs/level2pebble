8b03a610a7c5c07f07de991f0a9251168cfbd088
Tuan Pham Anh <baabeetaa@gmail.com>
2022-12-12 09:38:23 +0700

    use batch (#1)
     use batch for faster insert

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
+	"github.com/cockroachdb/pebble"
+	"math"
+
+	levelopt "github.com/syndtr/goleveldb/leveldb/opt"
+	"github.com/syndtr/goleveldb/leveldb/util"
 	tmdb "github.com/tendermint/tm-db"
 	"os"
 	"path/filepath"
@@ -20,12 +26,28 @@ func main() {
 	dbDirSource := filepath.Dir(os.Args[1])
 	dbDirTarget := os.Args[2]
 
-	dbLev, errLev := tmdb.NewGoLevelDB(dbName, dbDirSource)
+	// options to disable compaction for goleveldb
+	levelOptions := levelopt.Options{
+		ReadOnly: true,
+		//CompactionTableSizeMultiplier: 2.0,
+	}
+	dbLev, errLev := tmdb.NewGoLevelDBWithOpts(dbName, dbDirSource, &levelOptions)
 	if errLev != nil {
 		panic(errLev)
 	}
 
-	dbPeb, errPeb := tmdb.NewPebbleDB(dbName, dbDirTarget)
+	// options to disable compaction for pebbledb
+	pebbleOptions := &pebble.Options{
+		L0CompactionFileThreshold:   math.MaxInt32,
+		L0CompactionThreshold:       math.MaxInt32,
+		L0StopWritesThreshold:       math.MaxInt32,
+		MaxConcurrentCompactions:    1,
+		DisableAutomaticCompactions: true,
+		MaxOpenFiles:                100,
+	}
+	pebbleOptions.Experimental.ReadCompactionRate = math.MaxInt32
+	pebbleOptions.EnsureDefaults()
+	dbPeb, errPeb := tmdb.NewPebbleDBWithOpts(dbName, dbDirTarget, pebbleOptions)
 
 	if errPeb != nil {
 		panic(errPeb)
@@ -36,30 +58,72 @@ func main() {
 		dbLev.Close()
 	}()
 
-	itr, itrErr := dbLev.Iterator(nil, nil)
-
-	if itrErr != nil {
-		panic(itrErr)
+	readOptions := levelopt.ReadOptions{
+		DontFillCache: true,
+		Strict:        levelopt.DefaultStrict,
 	}
+
+	itr := dbLev.DB().NewIterator(&util.Range{Start: nil, Limit: nil}, &readOptions)
+
+	defer func() {
+		// itr.Close()
+		itr.Release()
+	}()
 
 	offset := 0
 
-	for ; itr.Valid(); itr.Next() {
-		key := itr.Key()
-		value := itr.Value()
+	rawDBPebble := dbPeb.DB()
+	bat := rawDBPebble.NewBatch()
 
-		errSet := dbPeb.Set(key, value)
+	for itr.First(); itr.Valid(); itr.Next() {
+		offset++
+
+		key := cp(itr.Key())
+		value := cp(itr.Value())
+
+		errSet := bat.Set(key, value, pebble.Sync)
 		if errSet != nil {
 			panic(errSet)
 		}
 
-		offset++
+		if bat.Len() >= 107374182 { // 100 MB
+			str_hex_key := hex.EncodeToString(key)
+			fmt.Printf("processing %s: %d, key=%s\n", dbName, offset, str_hex_key)
 
-		if offset%1000000 == 0 {
-			fmt.Printf("processing %s: %d\n", dbName, offset)
+			errComit := bat.Commit(pebble.Sync)
+			if errComit != nil {
+				panic(errComit)
+			}
+
+			errFlush := rawDBPebble.Flush()
+			if errFlush != nil {
+				panic(errFlush)
+			}
+
+			bat.Reset()
+
 			runtime.GC() // Force GC
 		}
 	}
 
-	itr.Close()
+	// write the last batch
+	errComit := bat.Commit(pebble.Sync)
+	if errComit != nil {
+		panic(errComit)
+	}
+
+	errFlush := rawDBPebble.Flush()
+	if errFlush != nil {
+		panic(errFlush)
+	}
+
+	bat.Close()
+
+	fmt.Printf("Done!")
+}
+
+func cp(bz []byte) (ret []byte) {
+	ret = make([]byte, len(bz))
+	copy(ret, bz)
+	return ret
 }
